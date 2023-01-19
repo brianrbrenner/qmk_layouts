213171306c53573ddbd17e9d0f9e6f2d09c8874a
Brian Brenner <69250070+brianrbrenner@users.noreply.github.com>
2023-01-19 18:25:05 -0500

    upload keymap

---
+++ brenner_colemakdh/config.h
@@ -0,0 +1,8 @@
+// prevents layout mirroring on flash, use dfu-split-left and dfu-split-right as bootloader targets
+#define EE_HANDS
+
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
