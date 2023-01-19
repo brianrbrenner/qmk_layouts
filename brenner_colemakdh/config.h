4e53fa4ef8d5b95520bfe19fd7ad2397c104fb30
Brian Brenner <69250070+brianrbrenner@users.noreply.github.com>
2023-01-19 18:28:15 -0500

    Update config.h

--- brenner_colemakdh/config.h
+++ brenner_colemakdh/config.h
@@ -1,6 +1,9 @@
-// prevents layout mirroring on flash, use dfu-split-left and dfu-split-right as bootloader targets
-#define EE_HANDS
+/* 
+prevents layout mirroring on flash, use dfu-split-left and dfu-split-right as bootloader targets
+ex: qmk compile -kb ferris/sweep -km brenner-colemakdh:dfu-split-left
+*/
 
+#define EE_HANDS
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
